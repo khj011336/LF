2185338ab2efbca3d5f4a20c804c4d5b66efecea
khj011336 <58839352+khj011336@users.noreply.github.com>
2020-03-31 17:29:56 +0900

    ffmpeg jar 추가 및 자바 소스
    ffmpeg jar 추가 및 자바 소스

--- src/LECFLY/dao/impl/VideoTimeCut.java
+++ src/LECFLY/dao/impl/VideoTimeCut.java
@@ -1,9 +1,35 @@
 package LECFLY.dao.impl;
+import java.io.IOException;
 
-
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 
 public class VideoTimeCut {
 
-	
-	
+	public static String media_player_time() {
+		System.out.println("@@ media_player_time start @@");
+		String returnData = "0";
+
+		try {
+			FFprobe ffprobe = new FFprobe("C:\\ffmpeg-4.2.2-win64-static\\bin\\ffprobe.exe"); // window에 설치된  ffprobe.exe 경로
+			FFmpegProbeResult probeResult = ffprobe.probe("C:\\cooking.mp4"); // 동영상 경로
+			FFmpegFormat format = probeResult.getFormat();
+			double second = format.duration; // 초단위
+
+			returnData = second + "";
+			System.out.println("second==" + second);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("@@ media_player_time end @@");
+		}
+
+		return returnData;
+	}
+
+	public static void main(String[] args) throws IOException {
+		media_player_time();
+	}
 }
