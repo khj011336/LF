59bc9eff012410ddb29bd128df8b7fcf77885e5d
khj011336 <58839352+khj011336@users.noreply.github.com>
2020-04-16 11:03:32 +0900

    video test
    aavideo test

--- src/LECFLY/dao/impl/AutopathFFmpeg.java
+++ src/LECFLY/dao/impl/AutopathFFmpeg.java
@@ -1,49 +1,28 @@
 package LECFLY.dao.impl;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.logging.Logger;
 
-
-	public class AutopathFFmpeg {
-
-		
-
+public class AutopathFFmpeg {
 	
-		
 
-
-		
-		public static void thumbnail2() {
-//			Runtime run = Runtime.getRuntime(); 
-//			try{ 
-//			    run.exec("C:\\windows\\explorer.exe"); 
-//			}catch(Exception e){ 
-//			    System.out.println("error : "+ e.getMessage()); 
-//			    e.printStackTrace(); 
-//			} 
-			Runtime run2 = Runtime.getRuntime();
-			String videofile = "C:\\cooking.mp4";
-			String command = "C:\\ffmpeg-4.2.2-win64-static\\bin\\ffmpeg.exe -i \""+videofile+"\" -ss 00:05:00 -vcodec png -vframes 1 \""+videofile+"_%2d.png\"";
-
-			try{
-			    run2.exec("cmd.exe chcp 65001");  // cmd에서 한글문제로 썸네일이 만들어지지않을시 cmd창에서 utf-8로 변환하는 명령
-			    run2.exec(command);
-			}catch(Exception e){
-			    System.out.println("error : "+e.getMessage());
-			    e.printStackTrace();
-			}             
-System.out.println("완료");
-
-
-		}
-	
-	
-	public static void main(String[] args) {
-		AutopathFFmpeg dd = new AutopathFFmpeg();
-		
-		dd.thumbnail2();
-		}
+	public static void main(String[] args) throws IOException {
+		thumbnail();
 	}
+	
+	
+	public static void thumbnail() {
+		Runtime run = Runtime.getRuntime();
+		String videofile = "C:\\cooking.mp4";
+		String command = "C:\\ffmpeg-4.2.2-win64-static\\bin\\ffmpeg.exe -i \"" + videofile + "\" -ss 00:00:01 -vcodec png -vframes 1 \""  +videofile + "_%2d.png\""; // 동영상 1초에서 Thumbnail 추출
+		System.out.println(command);
+		try{
+		    run.exec("cmd.exe chcp 65001");  // cmd에서 한글문제로 썸네일이 만들어지지않을시 cmd창에서 utf-8로 변환하는 명령
+		    run.exec(command);
+		}catch(Exception e){
+		    System.out.println("error : "+e.getMessage());
+		    e.printStackTrace();
+		}       
+
+
+	}
+}
